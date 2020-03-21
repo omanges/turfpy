529857049de511cbdf5894d9df728e04ee4651a1
omestry <omkar.mestry@here.com>
2020-03-21 18:18:42 +0530

    Version changes

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from codecs import open
 from os import path
 from setuptools import setup, find_packages
 
-__version__ = "0.0.4"
+__version__ = "0.0.1"
 
 here = path.abspath(path.dirname(__file__))
 
