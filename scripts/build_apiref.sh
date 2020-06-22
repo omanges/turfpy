32c7be7dcd5b191a3db4365978fdfdfe8ad81397
Kharude, Sachin <sachin.kharude@here.com>
2020-06-22 23:05:04 +0530

    docs changes
    Signed-off-by: Kharude, Sachin <sachin.kharude@here.com>

--- scripts/build_apiref.sh
+++ scripts/build_apiref.sh
@@ -8,10 +8,10 @@ rm -rf $DEST/_templates
 
 # Just creating conf.py, Makefile and make.bat once, hence commenting below.
 
-sphinx-quickstart --quiet --author 'Omkar Mestry, Sachin Kharude' --project turfpy \
-	--ext-coverage --ext-autodoc --ext-viewcode --ext-doctest \
-	--extensions sphinx_autodoc_typehints \
-	$DEST
+#sphinx-quickstart --quiet --author 'Omkar Mestry, Sachin Kharude' --project turfpy \
+#	--ext-coverage --ext-autodoc --ext-viewcode --ext-doctest \
+#	--extensions sphinx_autodoc_typehints \
+#	$DEST
 
 sphinx-apidoc --private --separate --module-first --full -o $DEST turfpy
 sphinx-build -b html -D html_theme=sphinx_rtd_theme $DEST $DEST/_build/html
