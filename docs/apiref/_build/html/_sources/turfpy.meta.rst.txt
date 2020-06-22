225e5ff0f4aecd693148d5b847ba3c917482daed
Kharude, Sachin <sachin.kharude@here.com>
2020-06-22 23:02:40 +0530

    updated docs changes
    Signed-off-by: Kharude, Sachin <sachin.kharude@here.com>

--- docs/apiref/_build/html/_sources/turfpy.meta.rst.txt
+++
@@ -1,8 +0,0 @@
-turfpy.meta module
-==================
-
-.. automodule:: turfpy.meta
-   :members:
-   :undoc-members:
-   :show-inheritance:
-   :private-members:
--- scripts/build_apiref.sh
+++ scripts/build_apiref.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export DEST=docs/apiref
+export DEST=docs/
 rm -f $DEST/*.rst
 rm -rf $DEST/_build
 rm -rf $DEST/_static
@@ -8,10 +8,10 @@ rm -rf $DEST/_templates
 
 # Just creating conf.py, Makefile and make.bat once, hence commenting below.
 
-#sphinx-quickstart --quiet --author 'Omkar Mestry, Sachin Kharude' --project turfpy \
-#	--ext-coverage --ext-autodoc --ext-viewcode --ext-doctest \
-#	--extensions sphinx_autodoc_typehints \
-#	$DEST
+sphinx-quickstart --quiet --author 'Omkar Mestry, Sachin Kharude' --project turfpy \
+	--ext-coverage --ext-autodoc --ext-viewcode --ext-doctest \
+	--extensions sphinx_autodoc_typehints \
+	$DEST
 
 sphinx-apidoc --private --separate --module-first --full -o $DEST turfpy
 sphinx-build -b html -D html_theme=sphinx_rtd_theme $DEST $DEST/_build/html
