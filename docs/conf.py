9ca42fa48b079773f3d10363f755ee600ea4002a
Kharude, Sachin <sachin.kharude@here.com>
2020-06-22 23:21:11 +0530

    removed typehints req

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.doctest',
-    'sphinx_autodoc_typehints',
+    # 'sphinx_autodoc_typehints',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
