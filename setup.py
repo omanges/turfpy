3faffb388c2469f0c84ec50da4f585a8042fc667
Kharude, Sachin <sachin.kharude@here.com>
2020-10-18 10:52:36 +0530

    made version accessible at runtime
    Signed-off-by: Kharude, Sachin <sachin.kharude@here.com>

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ from os import path
 
 from setuptools import find_packages, setup
 
-__version__ = "0.0.4"
-
 needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
 pytest_runner = ["pytest-runner"] if needs_pytest else []
 
@@ -17,6 +15,16 @@ with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
     all_reqs = f.read().split("\n")
 install_requires = [x.strip() for x in all_reqs if "git+" not in x]
 
+packages = find_packages(exclude=["docs", "tests"])
+version = {}
+with open('{}/__version__.py'.format(packages[0])) as f:
+    exec(f.read(), version)
+
+download_url = (
+    "https://github.com/omanges/turfpy"
+    "/archive/" + version['__version__'] + ".zip"
+)
+
 # The directory containing this file
 HERE = path.abspath(path.dirname(__file__))
 
@@ -26,11 +34,11 @@ with open(path.join(HERE, "README.md")) as fid:
 
 setup(
     name="turfpy",
-    version=__version__,
+    version=version['__version__'],
     description="A Python library for performing geospatial data analysis which reimplements turf.js.",
     long_description=README,
     long_description_content_type="text/markdown",
-    # download_url=download_url,
+    download_url=download_url,
     license="MIT",
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -47,7 +55,7 @@ setup(
         "Documentation": "https://turfpy.readthedocs.io",
         "Source": "https://github.com/omanges/turfpy"
     },
-    packages=find_packages(exclude=("tests",)),
+    packages=packages,
     keywords="Python Library for Turf",
     include_package_data=True,
     author="Omkar Mestry, Sachin Kharude",
