3f4b245baed62f8d38188c6e2d47a83c152b9343
Michael Keller <michael.keller.dc7d@statefarm.com>
2024-11-06 11:59:14 -0600

    build: require geopandas

--- setup.py
+++ setup.py
@@ -47,8 +47,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
